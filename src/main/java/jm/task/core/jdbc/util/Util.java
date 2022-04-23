e22e3b7ff931dcbe20ec73a5813ff1a5c89da18f
Kate <arsenjeva.ea@gmail.com>
2022-04-23 22:42:25 +0700

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,12 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    Connection connection;
+
+
+
+}
