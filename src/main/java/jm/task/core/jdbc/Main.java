2c346e9047d327d2996d7a6291cbfff1491f936c
Kate <arsenjeva.ea@gmail.com>
2022-04-25 02:52:47 +0700

    Исправлено по комменту ментора

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,21 +10,21 @@ import java.sql.DatabaseMetaData;
 public class Main {
     public static void main(String[] args) {
         // создание таблицы User ов (не должно быть исключения, если таблица уже есть)
-        UserService userRoot = new UserServiceImpl();
-        userRoot.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
         // добавление 4х юзеров в таблицу с данными на выбор
-        userRoot.saveUser("Valentin", "Ivanov", (byte) 20);
-        userRoot.saveUser("Ivan", "Petrov", (byte) 22);
-        userRoot.saveUser("Petr", "Sidorov", (byte) 23);
-        userRoot.saveUser("Sidr", "Smirnov", (byte) 40);
+        userService.saveUser("Valentin", "Ivanov", (byte) 20);
+        userService.saveUser("Ivan", "Petrov", (byte) 22);
+        userService.saveUser("Petr", "Sidorov", (byte) 23);
+        userService.saveUser("Sidr", "Smirnov", (byte) 40);
         // получение всех User из базы и вывод в консоль
-        System.out.println(userRoot.getAllUsers());
+        System.out.println(userService.getAllUsers());
         // удаление User из таблицы по id
-        userRoot.removeUserById(1);
+        userService.removeUserById(1);
         // очистка таблицы User ов
-        userRoot.cleanUsersTable();
+        userService.cleanUsersTable();
         // удаление таблицы (не должно быть исключения, если таблицы нет)
-        userRoot.dropUsersTable();
+        userService.dropUsersTable();
 
 
     }
